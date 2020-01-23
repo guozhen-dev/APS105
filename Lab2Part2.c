1592c95485eae0f8d07622852024c646f8dd9625
neyctech <39869385+neyctech@users.noreply.github.com>
2020-01-22 22:37:36 -0500

    Add comments for submit in order to get marks

--- Lab2Part2.c
+++ Lab2Part2.c
@@ -9,16 +9,20 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	// Get input from user 
 	int input;
 	printf("Enter an encrypted 4-digit combination:");
 	scanf("%d",&input);
+	
+	//Seperate the four digit from the input value
 	int firstDigit = input / 1000;
 	input -= firstDigit*1000;
 	int secondDigit = input / 100;
 	input -= secondDigit * 100;
 	int thirdDigit = input / 10 ;
 	input -= thirdDigit * 10 ;
+	
+	//re-combine the 4 digit and output to user 
 	int ans = input * 1000 + firstDigit + (9 - secondDigit) * 100 + (9 - thirdDigit) * 10 ;
 	printf("The real combination is: %04d\n", ans);
 	
