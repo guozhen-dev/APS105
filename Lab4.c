6a8c913eb2b3fc1abe8eec23344efd52e6247efb
Guozhen Ding <gzh.ding@outlook.com>
2020-01-29 15:42:25 -0500

    Update Lab4

--- Lab4.c
+++ Lab4.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char ** argv[]){
+int main(int argc, char const * argv[]){
+	int initAmount, payPM, purPM, yearIR, N;
+	
+	printf("Enter amount already owed on the card: ");
+	scanf("%d",&initAmount);
+	printf("Enter purchase amount per month: ");
+	scanf("%d",&payPM);
+	printf("Enter payment amount per month ");
+	scanf("%d",&purPM);
+	printf("Enter yearly interest rate as percent: ");
+	scanf("%d",&yearIR);
+	printf("Enter number of months to consider:");
+	scanf("%d",&N);
 	
 }
