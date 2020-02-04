1b8f3de7b5826ba5354865b53d3d3780b0a23ce6
Guozhen Ding <gzh.ding@outlook.com>
2020-02-04 15:02:14 -0500

    Add lab5

--- Lab4.c
+++ Lab4.c
@@ -1,8 +1,17 @@
+/* Name : Guozhen Ding (1005760749)
+ * Lab Submission for APS105H1
+ * Input : The amount already owed on card,purchase amount per month,payment amount per month, \
+ *         Enter yearly interest rate as percent, number of months to consider
+ * Output : Total purchased value: & Total interest charges:
+ * Assumption : All the input from the user is positive value and the months is an interger.
+ */
 #include <stdio.h>
 #include <math.h>
 
 double tot_int=0;
-const double eps = 1e-4;
+const double eps = 1e-4;								// declare global vars and constant
+
+
 double owedAmount(double interest, double owedFromPre, double Purchase, double Payment){
 	double ret;
 	if (owedFromPre > 0 ){
@@ -17,9 +26,11 @@ double owedAmount(double interest, double owedFromPre, double Purchase, double P
 		tot_int += ((Purchase - Payment)>0?(Purchase - Payment):0) * interest;
 	}
 	return ret; 
-}
+}     													// owed money process function 
 
 int main(int argc, char const * argv[]){
+
+	//get user input 
 	double last_owe,last_last_owe, payPM, purPM, interest,tot;
 	long long  N;
 	printf("Enter amount already owed on card: ");
@@ -33,11 +44,15 @@ int main(int argc, char const * argv[]){
 	interest/=1200;
 	printf("Enter number of months to consider: ");
 	scanf("%lld",&N);
+
+	//iterative call the function 
 	tot = last_owe;
 	for (int i = 0; i < N; i++){
 		last_last_owe = last_owe;
 		last_owe = owedAmount(interest,last_owe,purPM,payPM);
 	}
+
+	//Output the answer.
 	printf("Total purchased value: %.2lf\n", N*purPM+tot );
 	printf("Total interest charges: %.2lf\n", tot_int );
 }
