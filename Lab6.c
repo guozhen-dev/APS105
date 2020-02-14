6525319759af102b9818069ddcdcdb99368339b8
Guozhen Ding <gzh.ding@outlook.com>
2020-02-14 01:45:28 -0500

    Add Lab6

--- Lab6.c
+++ Lab6.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include "Lab6.h"
+
+const move[3]={-1,0,1};
+long long r,c;
+double p;
+void pr_number(int i , int j , int mapp[]){
+	int sum = 0 ;
+	for (int k = 0 ; k < 3; k++){
+		for (int l = 0 ; l < 3 ; l++){
+			if ((!i && !k) || (!j && !l) || (i==r-1 && k==2)||(j==c-1 && l==2) || (k==1 && l==1)) continue; 
+			sum += ( mapp[i+move[k]] >> (c-(j+move[l])-1) )%2;
+		}
+	}
+	printf("%d ",sum);
+}
 
 int main(int argc, char const *argv[])
 {
-	srand(time(0));
-	long long r,c;
-	double p;
+	freopen("in","r",stdin);
 	printf("Enter the number of rows:\nEnter the number of columns:\nEnter the probability p value:\n");
 	scanf("%lld %lld %lf", &r, &c, &p);
 	int mapp[0x7ffff] = {0,};
 	for (register int i = 0; i < 2*c-1 ; i++) putchar('-');
-	puts("");
+	putchar('\n');
 	for (register int i = 0; i < r ; i++){
 		for (register int j = 0 ; j < c ; j++){
-			int result = rand()/(double)RAND_MAX > p ? 1 : 0;
+			int result = rand() < p ? 1 : 0;
 			if (result) putchar('*'),putchar(' '); else putchar('.'),putchar(' ');
-			mapp[i] += result<<(c-j) ;
+			mapp[i] += result<<(c-j-1) ;
 		}
-		puts(' ');
+		putchar('\n');
 	}
-	for (register int i = 0; i < 2*c-1 ; i++) puts("-");
+	for (register int i = 0; i < 2*c-1 ; i++) putchar('-');
 	puts("\n");
+	for (int i = 0 ; i < r; i++){
+		for (int j = 0 ; j < c ; j++){
+			if ((mapp[i]>>(c-j-1)) % 2) {
+				putchar('*'),putchar(' ');
+				continue;
+			}else{
+				pr_number(i,j,mapp);
+			}
+
+		}
+		puts("");
+	}
 	return 0;
 }
