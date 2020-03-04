aec8ca0dbf147b199d805a210253d935ad5eb3bc
Guozhen Ding <gzh.ding@outlook.com>
2020-03-04 12:22:08 -0500

    Fix Lab6

--- Lab6.c
+++ Lab6.c
@@ -2,23 +2,22 @@
 #include <string.h>
 #include "Lab6.h"
 
-const move[3]={-1,0,1};
+const int move[3]={-1,0,1};
 long long r,c;
 double p;
 void pr_number(int i , int j , int mapp[]){
-	int sum = 0 ;
+	long long  sum = 0 ;
 	for (int k = 0 ; k < 3; k++){
 		for (int l = 0 ; l < 3 ; l++){
 			if ((!i && !k) || (!j && !l) || (i==r-1 && k==2)||(j==c-1 && l==2) || (k==1 && l==1)) continue; 
 			sum += ( mapp[i+move[k]] >> (c-(j+move[l])-1) )%2;
 		}
 	}
-	printf("%d ",sum);
+	printf("%lld ",sum);
 }
 
 int main(int argc, char const *argv[])
 {
-	// freopen("in","r",stdin);
 	printf("Enter the number of rows: Enter the number of columns: Enter the probability p value: ");
 	scanf("%lld %lld %lf", &r, &c, &p);
 	int mapp[0x7ffff] = {0,};
@@ -33,7 +32,7 @@ int main(int argc, char const *argv[])
 		putchar('\n');
 	}
 	for (register int i = 0; i < 2*c-1 ; i++) putchar('-');
-	puts("\n");
+	puts("");
 	for (int i = 0 ; i < r; i++){
 		for (int j = 0 ; j < c ; j++){
 			if ((mapp[i]>>(c-j-1)) % 2) {
@@ -42,7 +41,6 @@ int main(int argc, char const *argv[])
 			}else{
 				pr_number(i,j,mapp);
 			}
-
 		}
 		puts("");
 	}
