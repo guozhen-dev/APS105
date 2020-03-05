b6dbac88b5afbe14390d136491378848ea0d7f63
Guozhen Ding <gzh.ding@outlook.com>
2020-03-05 15:44:15 -0500

    Remove rubbish and add comments to Lab6

--- Lab6.c
+++ Lab6.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include "Lab6.h"
+
+//def a function for printing the lines
 void printline(long long  x){
 	for (long long i = 0 ; i< 2*x-1 ; i++){
 		putchar('-');
 	}
 	puts("");
-}
+} 
+
 int main(int argc, char const *argv[])
 {
-	// freopen ("in","r",stdin);
+	#ifdef DEBUG
+	freopen ("in","r",stdin);
+	#endif 
 	long long r,c;
 	double p;
 	printf("Enter the number of rows: \nEnter the number of columns: \nEnter the probability p value: \n");
 	scanf("%lld %lld %lf",&r,&c,&p);
+	//ask user to enter the information needed 
+	
 	int ** mapp = (int ** )malloc( sizeof (int * ) * (r+2));
 	for  (register long long  i = 0 ; i < r + 2 ; i ++){
 		mapp[i] = (int * ) malloc (sizeof (int ) * (c+2));
@@ -23,7 +30,11 @@ int main(int argc, char const *argv[])
 			mapp[i][j] = 0;
 		}
 	}
+	//alloc and init the mapp memory
+	
 	printline(c);
+	//print the dash lines 
+	
 	for ( register long long  i = 1; i < r + 1 ; i++){
 		for (register long long  j = 1 ; j < c+1 ; j++){
 			mapp[i][j] = rand()>p ? 0 : 1;
@@ -31,7 +42,11 @@ int main(int argc, char const *argv[])
 		}
 		puts("");
 	}
+	//fill in the nessary place in the memory 
+	
 	printline(c);
+	//print dash lines 
+	
 	for ( register long long  i = 1; i < r + 1 ; i++){
 		for (register long long  j = 1 ; j < c+1 ; j++){
 			if (mapp[i][j]) putchar('*'),putchar(' ');
@@ -41,7 +56,10 @@ int main(int argc, char const *argv[])
 		}
 		puts("");
 	}
+	//calculate the numbers of bombs around a point
+	
 	printline(c);
+	//print the last dash lines 
 
 
 	return 0;
